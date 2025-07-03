cbfd4aa8d3a032bb1dd3c64981826d1329ba9f2d
anggadwiiic <24081010127@student.upnjatim.ac.id>
2025-07-03 14:45:53 +0700

    Update proyek student profile tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Components/Header'; // Import komponen Header
-import Button from './Components/Button'; // Import komponen Header
+import './App.css';
+import Header from './Components/Header';
+import Button from './Components/Button';
+import TopNav from './Components/TopNav';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header title="PROGRESS TRACKER"/>
-      {/* Konten lainnya bisa ditambahkan di sini */}
-      <Button text="PROFIL MAHASISWA"/>
+      <TopNav />
+      <Header title="PROGRESS TRACKER" />
+      <Button text="PROGRESS ANDA" />
+
       <main>
-        <p>Ini adalah konten utama aplikasi.</p>
+        
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/TopNav.jsx
@@ -0,0 +1,11 @@
+import './TopNav.css';
+
+export default function TopNav() {
+  return (
+    <div className="topnav-wrapper">
+      <button>DASHBOARD</button>
+      <button>MATERI BELAJAR</button>
+      <button>PENCAPAIAN</button>
+    </div>
+  );
+}
